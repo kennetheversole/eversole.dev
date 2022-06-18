d23853ed4b043fbcccbe944261f75517e7a8c289
Kenneth Eversole <kenneth.eversole@gmail.com>
2022-06-18 15:35:52 -0500

    color mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import {ChakraProvider, Flex} from '@chakra-ui/react'
+import Navbar from '../components/Navbar'
+import { ColorModeScript } from '@chakra-ui/react'
+import theme from '../styles/theme'
+import { extendTheme } from '@chakra-ui/react'
+
+
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+// 2. Extend the theme to include custom colors, fonts, etc
+ const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+  }
+
+ const theme = extendTheme({ colors })
+
+
+
+
+ return (
+    <ChakraProvider theme={theme}>
+        <Head>
+        <title> Eversole.dev</title>
+      </Head>
+      <Navbar></Navbar>
+      <Flex
+        w="100%"
+        px="15vw">
+      <Component {...pageProps }/>
+     </Flex>
+  </ChakraProvider>
+   )
 }
 
 export default MyApp
---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import { Center, Flex } from "@chakra-ui/react"
+
+
+
+const About: nextpage= () => {
+    return (
+        <Center>
+           About me
+       </Center>
+
+    )
+
+
+}
+
+
+export default About
