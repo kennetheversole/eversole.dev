07b238665c2dedfd11e3acada08575ea4fecae48
Kenneth Eversole <kenneth.eversole@gmail.com>
2024-03-06 19:04:31 -0600

    feature: change color to black

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ fn App() -> Html {
          <div>
             <h1>{"Hello, Welcome!"}</h1>
             <p>{"My name is Kenneth and this is my little part of the internet"}</p>
+            <h3>{"Contact me!"}</h3>
+            <p>
+                <ul>                 
+                    <li><a href="mailto:keversole@eversole.dev">{"Kenneth@eversole.dev"}</a></li>
+                    <li><a href="https://github.com/kennetheversole">{"Github"}</a></li>
+                </ul>
+            </p>
          </div>
      </>
     }
